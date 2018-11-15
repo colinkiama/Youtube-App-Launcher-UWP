69840d0baffccbb67d847cb249aa2966c5aa18b3
Colin Kiama <colinkiama@hotmail.co.uk>
2018-11-15 14:45:38 +0000

    Now uses regex to get videoID from Youtube URLs. Can pretty much handle any youtube video url now.
    If a URL doesn't work correctly then you can simply updated the MatchService class. In most cases you should only need to update the 'videoIDMatchRegexString'.
    
    Feels great to finally come back and improve this app 😊

--- launchYoutubeInApp/MainPage.xaml.cs
+++ launchYoutubeInApp/MainPage.xaml.cs
@@ -14,134 +14,37 @@ namespace launchYoutubeInApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-       public static string url = "";
+        MatchService matchService = new MatchService();
+
+        public static string url = "";
         public MainPage()
         {
             this.InitializeComponent();
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            url = (string)e.Parameter;
-
-           
-            
-        }
-
-
-       
-
-        private string getVideoIDFromUrl(string url)
-        {
-
-            string videoID = "";
-
-
-            if (url.Contains("http"))
-            {
-                if (url.Contains("https"))
-                {
-                    url = url = url.Replace("https://", "");
-                }
-                else
-                {
-                 url = url.Replace("http://", "");
-                }
-            }
-
-           
-            //and return nothing in final else
-
-
-
-            if (url.Contains("www"))
-            {
-                url = url.Replace("www.", "");
-            }
-
-            //Handle all m.youtube cases in one if
-            //all youtube cases in else if
-            //all youtu.be in another else if
-
-            if (url.Contains("m.youtube.com"))
-            {
-                if (url.Contains("m.youtube.com/embed"))
-                {
-                    videoID = url.Replace("m.youtube.com/embed/", "");
-                }
-                else
-                {
-                    videoID = url.Replace("m.youtube.com/watch?v=", "");
-                }
-            }
-
-            else if (url.Contains("youtube.com"))
-            {
-                if (url.Contains("youtube.com/embed"))
-                {
-                    videoID = url.Replace("youtube.com/embed/", "");
-                }
-                else
-                {
-                    videoID = url.Replace("youtube.com/watch?v=", "");
-                }
-
-            }
-
-
-
-
-
-
-
-
-            else if (url.Contains("youtu.be"))
-            {
-                videoID = url.Replace("youtu.be/", "");
-
-            }
-
-            else videoID = $"{url} is invalid.";
-
-            byte videoIDLength = (byte)videoID.Length;
-            if (videoID[videoIDLength - 2] == '?' && videoID[videoIDLength - 1] == 'a')
-            {
-                videoID = videoID.Remove(videoIDLength - 2, 2);
-            }
-
-            if (videoID.Contains("?autoplay"))
-            {
-                int startOfRemoval = videoID.LastIndexOf("?autoplay");
-                videoID = videoID.Remove(startOfRemoval);
-            }
-
-
-
-
-            return videoID;
+            url = (string)e.Parameter;            
         }
 
         private async void launchmyTubeButton_Click(object sender, RoutedEventArgs e)
         {
-            //Testing 
-            //string videoID = getVideoIDFromUrl("https://www.youtube.com/watch?v=9bDEQJ2w4KA");
+            //Testing
+            //string videoID = matchService.Match("https://www.youtube.com/watch?v=9bDEQJ2w4KA");
 
-
-            string videoID = getVideoIDFromUrl(url);
+            string videoID = matchService.Match(url);
             await Launcher.LaunchUriAsync(new Uri($"rykentube:Video?ID={videoID}"));
             Application.Current.Exit();
-            //await Launcher.LaunchUriAsync(new Uri("mytube://"));
+            
         }
 
         private async void launchPerfectTubeButton_Click(object sender, RoutedEventArgs e)
         {
-            //Testing 
-            //string videoID = getVideoIDFromUrl("https://www.youtube.com/watch?v=9bDEQJ2w4KA");
+            //Testing
+            //string videoID = matchService.Match("https://www.youtube.com/watch?v=9bDEQJ2w4KA");
 
-
-            string videoID = getVideoIDFromUrl(url);
+            string videoID = matchService.Match(url);
             await Launcher.LaunchUriAsync(new Uri($"perfecttube:{videoID}"));
             Application.Current.Exit();
-            //await Launcher.LaunchUriAsync(new Uri("mytube://"));
         }
     }
 }
---
+++ launchYoutubeInApp/MatchService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace launchYoutubeInApp
+{
+    public class MatchService
+    {
+        const string videoIDMatchRegexString = "(?:youtube\\.com\\/(?:[^\\/]+\\/.+\\/|(?:v|e(?:mbed)?)\\/|.*[?&]v=)|youtu\\.be\\/)([^\" &?\\/ ]{11})";
+
+        public const string notFoundVideoID = "noVideoFound";
+        Regex rx;
+
+        public MatchService()
+        {
+            rx = new Regex(videoIDMatchRegexString, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
+
+        public string Match(string videoUrl)
+        {
+            string videoIDToReturn = notFoundVideoID;
+
+            MatchCollection matches = rx.Matches(videoUrl);
+
+            if (matches.Count > 0)
+            {
+                if (matches[0].Groups.Count > 1)
+                {
+                    // Because of the nature of the regex string
+                    // the video ID will always be in the second group (Index 1).
+                    videoIDToReturn = matches[0].Groups[1].Value;
+
+                }
+            }
+
+            return videoIDToReturn;
+        }
+    }
+}
